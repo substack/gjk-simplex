1c26d2e503ed978a8d483bbfbb7a56335c02e560
James Halliday <mail@substack.net>
2015-02-09 04:23:57 -0800

    complete rewrite based on dyn4j writeup

--- index.js
+++ index.js
@@ -4,72 +4,75 @@ var norm = require('gl-matrix').vec3.normalize;
 var cross = require('gl-matrix').vec3.cross;
 
 var origin = [0,0,0];
-var nb = [[0,0,0],[0,0,0],[0,0,0]];
-var nD = [0,0,0];
-var tmpa = [0,0,0];
-var tmpb = [0,0,0];
-var tmpc = [0,0,0];
-var pts = [null,null,null,null];
 
 module.exports = function (a, b) {
-    sub(nb[0], origin, b[0]);
-    sub(nb[1], origin, b[1]);
-    sub(nb[2], origin, b[2]);
-    
-    var mdiff = msum(a, nb);
-    
+    var simplex = [];
     var D = [ 0, 0, 1 ];
-    var S = support(tmpa, D, a, b);
-    sub(D, origin, S);
-    var pts = [ S ];
     
-    for (var i = 1; i <= 10; i++) {
-        var A = support(tmpa, D, a, b);
-console.log(A); 
-        if (dot(A, D) < 0) return null; // no intersection
-        pts.push(copy(A));
-        var r = evolve(tmpa, pts[pts.length-1], pts[pts.length-2], D);
-        if (r) {
-            console.log('!!!', r);
-            break;
-        }
-        else {
-            sub(D, origin, pts[pts.length-1]);
-        }
-    }
+    simplex.push(support([], D, a, b));
+    negate(D);
     
-    return mdiff;
+    var max = a.length * b.length;
+    for (var i = 0; i <= max; i++) {
+        simplex.push(support([], D, a, b));
+        if (dot(simplex[simplex.length-1], D) <= 0) {
+            return null;
+        }
+        if (containsOrigin(simplex, D)) {
+            return simplex;
+        }
+    } 
+    return null;
 };
 
-function evolve (out, a, b, D) {
-    var ab = sub(tmpa, b, a);
-    var a0 = sub(tmpb, origin, a);
-    if (dot(ab, a0) > 0) {
-        cross(out, ab, cross(tmpc, a0, ab));
-        
-        if (out[0] === 0 && out[1] === 0 && out[2] === 0) {
-            // perpendicular trick
-            out[0] = -ab[1];
-            out[1] = ab[0];
-            out[2] = 0;
-            norm(out, out);
-        }
-        return out;
+function containsOrigin (simplex, D) {
+    if (simplex.length === 2) return contains2(simplex, D);
+    if (simplex.length === 3) return contains3(simplex, D);
+    return null;
+}
+
+function contains2 (simplex, D) {
+    var A = simplex[simplex.length-1];
+    var B = simplex[simplex.length-2];
+    var AB = sub([], B, A);
+    var A0 = sub([], origin, A);
+    cross(D, cross([], AB, A0), AB);
+    if (dot(D, A0) < 0) return false;
+    return true;
+}
+
+function contains3 (simplex, D) {
+    var A = simplex[simplex.length-1];
+    var B = simplex[simplex.length-2];
+    var C = simplex[simplex.length-3];
+    var AB = sub([], B, A);
+    var AC = sub([], C, A);
+    var A0 = sub([], origin, A);
+    
+    var abp = cross([], cross([], AC, AB), AB);
+    var acp = cross([], cross([], AB, AC), AC);
+    if (dot(abp, A0) > 0) {
+        simplex.splice(simplex.length - 3, 1); // remove C
+        set(D, abp);
     }
+    else if (dot(acp, A0) > 0) {
+        simplex.splice(simplex.length - 2, 1); // remove B
+        set(D, acp);
+    }
+    else return true;
 }
 
 function support (out, D, a, b) {
-    var maxa = -Infinity;
-    var maxb = -Infinity;
+    var maxa = -Infinity, maxb = -Infinity;
     var pa = null, pb = null;
     
-    for (var i = 1; i < a.length; i++) {
+    for (var i = 0; i < a.length; i++) {
         var ma = dot(D, a[i]);
-        var mb = -dot(D, b[i]);
         if (ma > maxa) {
             pa = a[i];
             maxa = ma;
         }
+        var mb = -1 * dot(D, a[i]);
         if (mb > maxb) {
             pb = b[i];
             maxb = mb;
@@ -78,4 +81,16 @@ function support (out, D, a, b) {
     return sub(out, pa, pb);
 }
 
-function copy (x) { return x.slice() }
+function negate (x) {
+    x[0] = -x[0];
+    x[1] = -x[1];
+    x[2] = -x[2];
+    return x;
+}
+
+function set (a, b) {
+    a[0] = b[0];
+    a[1] = b[1];
+    a[2] = b[2];
+    return a;
+}
--- readme.markdown
+++ readme.markdown
@@ -1,5 +1,13 @@
 # gjk-simplex
 
+calculate the simplex of two shapes using the
+[Gilbert-Johnson-Keerthi distance algorithm][0]
+
+Implementation based on [this writeup by William Bittle][1].
+
+[0]: https://en.wikipedia.org/wiki/Gilbert%E2%80%93Johnson%E2%80%93Keerthi_distance_algorithm
+[1]: http://www.dyn4j.org/2010/04/gjk-gilbert-johnson-keerthi/
+
 # example
 
 ``` js
